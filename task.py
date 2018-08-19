6c189c90e83935576d8b8d0dc9f7c6ec7fa99b6b
jt <test@example.com>
2018-08-19 21:08:56 +1000

    Modified reward function to be linear on difference in z

--- task.py
+++ task.py
@@ -3,7 +3,7 @@ from physics_sim import PhysicsSim
 
 class Task():
     """Task (environment) that defines the goal and provides feedback to the agent."""
-    def __init__(self, init_pose=None, init_velocities=None, 
+    def __init__(self, init_pose=None, init_velocities=None,
         init_angle_velocities=None, runtime=5., target_pos=None):
         """Initialize a Task object.
         Params
@@ -15,7 +15,7 @@ class Task():
             target_pos: target/goal (x,y,z) position for the agent
         """
         # Simulation
-        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime)
         self.action_repeat = 3
 
         self.state_size = self.action_repeat * 6
@@ -24,12 +24,12 @@ class Task():
         self.action_size = 4
 
         # Goal
-        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
+        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.])
 
     def get_reward(self):
         # Calculate distance from target
         # dist_from_target = np.sqrt(np.square(self.sim.pose[:3] - self.target_pos).sum())
-        z_dist = np.absolute(self.sim.pose[2] - self.target_pos[2])**2
+        z_dist = np.absolute(self.sim.pose[2] - self.target_pos[2])
         non_z_dist = np.sqrt(np.square(self.sim.pose[:2] - self.target_pos[:2]).sum())
         # Want the quadcopter to hover straight up, measure angular deviation
         angular_dev = np.sqrt(np.square(self.sim.pose[3:]).sum())
@@ -38,7 +38,7 @@ class Task():
         # Also penalise movement downwards
         z_v = np.absolute(np.minimum(self.sim.v[2],0))
         # Penalty term
-        penalty = .000001*z_dist + .001*non_z_dist + .0005*angular_dev + .0005*non_z_v + 0.1*z_v
+        penalty = .001*z_dist + .001*non_z_dist + .0005*angular_dev + .0005*non_z_v + 0.1*z_v
         reward = 1. - penalty
         return reward
 
@@ -48,7 +48,7 @@ class Task():
         pose_all = []
         for _ in range(self.action_repeat):
             done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
-            reward += self.get_reward() 
+            reward += self.get_reward()
             pose_all.append(self.sim.pose)
         next_state = np.concatenate(pose_all)
         return next_state, reward, done
@@ -56,5 +56,5 @@ class Task():
     def reset(self):
         """Reset the sim to start a new episode."""
         self.sim.reset()
-        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        state = np.concatenate([self.sim.pose] * self.action_repeat)
         return state
